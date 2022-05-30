60d33e1bc2eadca2e20832b3b9675c623a9c9985
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-05-31 02:37:44 +0800

    commasasqaqwekk

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ const cors = require("cors");
 const server = http.createServer(app);
 const user = [];
 
+app.use(cors());
+
 const io = new Server(server, {
     cors: {
         origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000),
@@ -14,12 +16,12 @@ const io = new Server(server, {
     },
 });
 
-const corsOptions ={
-    origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000), 
-    credentials:true,            //access-control-allow-credentials:true
-    optionSuccessStatus:200
-}
-app.use(cors(corsOptions));
+// const corsOptions ={
+//     origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000), 
+//     credentials:true,            //access-control-allow-credentials:true
+//     optionSuccessStatus:200
+// }
+// app.use(cors(corsOptions));
 
 io.on("connection", (socket) => {
 
