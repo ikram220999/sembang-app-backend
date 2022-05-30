285b86473de16e7b5d37207b90ce3170605ccfb5
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-05-30 22:34:16 +0800

    make it better

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const { Server } = require("socket.io");
+const cors = require("cors");
+
+app.use(cors());
+
+const server = http.createServer(app);
+const user = [];
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    },
+});
+
+io.on("connection", (socket) => {
+
+    user.push(socket.id);
+   
+    // console.log(`User Connected ${socket.id}`);
+
+    socket.on("join_room", (data) =>{
+        socket.join(data);
+    })
+
+    socket.on("send_message", (data) => { 
+
+        let new_data = {user_id: data.id, msg: data.text};
+      socket.to(data.room).emit("receive_message", new_data);
+    })
+});
+
+server.listen(3001, () => {
+    console.log("SERVER OK");
+});
