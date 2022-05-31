dd8571159d46148cbf5b734f20aef124f23e1dc9
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-05-31 19:01:07 +0800

    qwe

--- index.js
+++ index.js
@@ -10,47 +10,46 @@ const user = [];
 app.use(cors());
 
 const io = new Server(server, {
-    cors: {
-        origin: `http://sembang-kari-app.herokuapp.com:${(process.env.PORT || 5000)}/`,
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: `http://sembang-kari-app.herokuapp.com:${
+      process.env.PORT || 5000
+    }/`,
+    methods: ["GET", "POST"],
+  },
 });
 
-app.get('*', (req, res) => {
-    res.send('helo');
+app.get("*", (req, res) => {
+    console.log("kambing");
+  io.on("connection", (socket) => {
+    user.push(socket.id);
+
+    // console.log(`User Connected ${socket.id}`);
+
+    socket.on("join_room", (data) => {
+      socket.join(data);
     });
 
+    socket.on("send_message", (data) => {
+      let new_data = { user_id: data.id, msg: data.text };
+      socket.to(data.room).emit("receive_message", new_data);
+    });
+  });
+});
+
 // const corsOptions ={
-//     origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000), 
+//     origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000),
 //     credentials:true,            //access-control-allow-credentials:true
 //     optionSuccessStatus:200
 // }
 // app.use(cors(corsOptions));
 
-io.on("connection", (socket) => {
-
-    user.push(socket.id);
-   
-    // console.log(`User Connected ${socket.id}`);
-
-    socket.on("join_room", (data) =>{
-        socket.join(data);
-    })
-
-    socket.on("send_message", (data) => { 
-
-        let new_data = {user_id: data.id, msg: data.text};
-      socket.to(data.room).emit("receive_message", new_data);
-    })
-});
-
 // server.listen("http:///" + process.env.PORT || 5000, () => {
 //     console.log("SERVER OK");
 // });
 
-const host = '0.0.0.0';
+const host = "0.0.0.0";
 const port = process.env.PORT || 3000;
 
-app.listen(port, host, function() {
-    console.log("Server started.......");
-  });
+app.listen(port, host, function () {
+  console.log("Server started.......");
+});
