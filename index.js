ff761803f98c5e555dde70291dc441bb254795a7
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-05-31 17:41:01 +0800

    qwe

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const http = require("https");
 const { Server } = require("socket.io");
 const cors = require("cors");
 
@@ -10,36 +10,31 @@ const user = [];
 app.use(cors());
 
 const io = new Server(server, {
-    cors: {
-        origin: "http://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000),
-        methods: ["GET", "POST"],
-    },
+  cors: {
+    origin: "http://test-app.herokuapp.com/" + (process.env.PORT || 5000),
+    methods: ["GET", "POST"],
+  },
 });
 
-// const corsOptions ={
-//     origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000), 
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+app.get("*", (req, res) => {
+  res.send("helo");
+});
 
 io.on("connection", (socket) => {
+  user.push(socket.id);
 
-    user.push(socket.id);
-   
-    // console.log(`User Connected ${socket.id}`);
+  // console.log(`User Connected ${socket.id}`);
 
-    socket.on("join_room", (data) =>{
-        socket.join(data);
-    })
+  socket.on("join_room", (data) => {
+    socket.join(data);
+  });
 
-    socket.on("send_message", (data) => { 
-
-        let new_data = {user_id: data.id, msg: data.text};
-      socket.to(data.room).emit("receive_message", new_data);
-    })
+  socket.on("send_message", (data) => {
+    let new_data = { user_id: data.id, msg: data.text };
+    socket.to(data.room).emit("receive_message", new_data);
+  });
 });
 
 server.listen(process.env.PORT || 5000, () => {
-    console.log("SERVER OK");
+  console.log("SERVER OK");
 });
