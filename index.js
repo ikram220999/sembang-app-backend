79edd9b700a9f134867637ceef96c683b9915b68
LAPTOP-K0PEIKNF\ikram <ikramhensem25@gmail.com>
2022-05-31 19:09:54 +0800

    qwe

--- index.js
+++ index.js
@@ -36,12 +36,14 @@ app.get("*", (req, res) => {
   });
 });
 
-// const corsOptions ={
-//     origin: "https://sembang-kari-app.herokuapp.com/" + (process.env.PORT || 5000),
-//     credentials:true,            //access-control-allow-credentials:true
-//     optionSuccessStatus:200
-// }
-// app.use(cors(corsOptions));
+const corsOptions ={
+    origin: `https://sembang-kari-app.herokuapp.com:${
+        process.env.PORT || 5000
+      }/`,
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+app.use(cors(corsOptions));
 
 // server.listen("http:///" + process.env.PORT || 5000, () => {
 //     console.log("SERVER OK");
